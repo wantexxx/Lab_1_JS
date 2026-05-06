08c6e6a0eaa1cef5cbc813ac5cd94034baf8b471
wantexxx <kirillsushko2007@gmail.com>
2026-05-06 22:26:27 +0300

    Update showWarning function to display name 'Кирило'

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// А. Виводимо "Hello world!" у заголовок h1 [cite: 10, 11]
-// Використовуємо метод querySelector 
+
 const title = document.querySelector('#myTitle');
 title.textContent = "Hello world!";
 
-// Б. Знаходимо кнопку за допомогою querySelector [cite: 7, 13]
 const btn = document.querySelector('#myBtn');
 
-// В. Створюємо функцію, яка виводить твоє ім'я як попередження (warn) 
 function showWarning() {
-    console.warn("Твоє Ім'я"); // Тут напиши своє ім'я
+    console.warn("Кирило"); 
 }
 
-// Г. Призначаємо івент onmousemove (коли мишка рухається над кнопкою) [cite: 8, 13]
 btn.onmousemove = showWarning;
